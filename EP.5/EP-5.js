e4ca7dac49c810a2111d0ffdadf97e2ff0641cf1
sufiyan <sufiyan.manuu@gmail.com>
2025-10-07 11:27:55 +0530

    Js By Akshay saini

---
+++ EP.5/EP-5.js
@@ -0,0 +1,20 @@
+/* SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5 */
+
+// let a = 5;
+
+// console.log(window.a)
+// console.log(this.a)
+
+
+
+/* Q--> 
+    If i initialize a variable with let , like 
+    let a=5;
+    console.log(window.a)
+    console.log(this.a)
+    Can anyone tell me what will happen and tell me why it happened with reason 
+
+    ans: 
+     Undefined for both console statements. This is because window & this points at the global object whereas let & const are assigned memory in a different space and Not the Global objects memory space.
+    
+    */
