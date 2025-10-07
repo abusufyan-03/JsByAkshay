6b6fb26e2a8e25e51bd7e261fa17fc30db23d020
sufiyan <sufiyan.manuu@gmail.com>
2025-10-07 11:57:18 +0530

    EP.2 to EP.5

---
+++ Season-1/EP.5/EP.4/EP-4.js
@@ -0,0 +1,25 @@
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+
+// function a(){
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b(){
+//     var x = 100;
+//     console.log(x)
+// }
+
+
+
+
+// a();
+// var a = () => {
+//     console.log("build in public")
+// }
+
+
+
