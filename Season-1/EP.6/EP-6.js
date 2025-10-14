81f438b33e7b20538cbff5e9def4927286a8fc64
sufiyan <sufiyan.manuu@gmail.com>
2025-10-14 22:56:07 +0530

    undefined vs not defined EP.6

---
+++ Season-1/EP.6/EP-6.js
@@ -0,0 +1,33 @@
+/* undefined vs not defined in JS 🤔 | Namaste JavaScript Ep. 6 */
+
+// console.log(a)
+// var a = 7;
+// console.log(a)
+// console.log(x)
+
+
+
+// var a;
+// console.log(a)
+// a = 10;
+// if (a === undefined){
+//     console.log("a is undefined");
+// } else {
+//     console.log('a is not undefined')
+// }
+
+
+/* loosly types language is also knowns as weakly type language*/
+// var a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// a = "hello world";
+// console.log(a);
+
+
+// a = undefined;
+/* we can assigned undefined to a variable but we should never do this
+it's not a good practice
+let it be used for that purpose only
+*/
